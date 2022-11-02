eb605cfbad2846c6f54be263247c621752aefd31
Urszula Molska <urszula_molska@wp.pl>
2022-11-02 18:54:52 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -5,62 +5,86 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('form');
 const gallery = document.querySelector('.gallery');
-let q;
 const photoCard = document.querySelector('.photo-card');
-const loadMore = document.querySelector('.load-more');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let q;
+let per_page = 40;
+let page = 1;
 
 form.addEventListener('submit', handlesubmit);
 
 function handlesubmit(event) {
   event.preventDefault();
-  let q = form.elements.searchQuery.value;
+  q = form.elements.searchQuery.value;
 
-  const params = new URLSearchParams({
-    key: '30974723-e837a19c04863567111943fb7',
-    q: q,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: 1,
-  });
-
-  const URL = `https://pixabay.com/api/?${params}`;
-
-  fetchPictures(URL).then(pictures => {
-    const hits = pictures.hits;
+  fetchPictures().then(pictures => {
     console.log(pictures);
-    console.log(hits);
     console.log(pictures.totalHits);
+    const totalPages = pictures.totalHits / per_page;
     if (pictures.totalHits > 0) {
-      let markup = '';
-      gallery.innerHTML = '';
-      hits.forEach(hit => {
-        markup += `<div class="photo-card" style="border:gainsboro;border-width:1px;border-style:solid;border-radius:5px"><a class="lightbox" href="${hit.largeImageURL}"><img style="object-fit:cover;" src="${hit.webformatURL}" alt=${hit.tags} loading="lazy" width=263px height="176px" 
-          /></a>
-             <div class="info"><p class="info-item"><b>Likes</b>${hit.likes}
-             </p><p class="info-item"><b>Views</b>${hit.views}</p><p class="info-item">
-             <b>Comments</b>${hit.comments}</p><p class="info-item"><b>Downloads</b>${hit.downloads}</p>
-             </div></div>`;
-      });
-      gallery.innerHTML = markup;
-      //return (gallery.innerHTML = markup);
+      renderImages(pictures);
 
-      let LightboxGallery = new SimpleLightbox('.gallery a');
-      LightboxGallery.on('show.simplelightbox');
-      LightboxGallery.defaultOptions.captionsData = 'alt';
-
-      LightboxGallery.defaultOptions.captionDelay = 250;
-      document.addEventListener('keyup', event => {
-        if (event.code === 'Escape') {
-          LightboxGallery.close;
+      loadMoreBtn.addEventListener('click', () => {
+        // Check the end of the collection to display an alert
+        if (page > totalPages) {
+          console.log('re sorry, there are no more posts to load');
         }
+
+        fetchPictures()
+          .then(pictures => {
+            renderImages(pictures);
+            // Increase the group number
+            page += 1;
+
+            // Replace button text after first request
+            if (page > 1) {
+              fetchPostsBtn.textContent = 'Fetch more posts';
+            }
+          })
+          .catch(error => console.log(error));
       });
     } else return console.log('0 records');
   });
 }
 
-async function fetchPictures(URL) {
+function renderImages(pictures) {
+  let markup = '';
+  gallery.innerHTML = '';
+  const hits = pictures.hits;
+  hits.forEach(hit => {
+    markup += `<div class="photo-card" style="border:gainsboro;border-width:1px;border-style:solid;border-radius:5px"><a class="lightbox" href="${hit.largeImageURL}"><img style="object-fit:cover;" src="${hit.webformatURL}" alt=${hit.tags} loading="lazy" width=263px height="176px" 
+          /></a>
+             <div class="info"><p class="info-item"><b>Likes</b>${hit.likes}
+             </p><p class="info-item"><b>Views</b>${hit.views}</p><p class="info-item">
+             <b>Comments</b>${hit.comments}</p><p class="info-item"><b>Downloads</b>${hit.downloads}</p>
+             </div></div>`;
+  });
+  gallery.innerHTML = markup;
+
+  let LightboxGallery = new SimpleLightbox('.gallery a');
+  LightboxGallery.on('show.simplelightbox');
+  LightboxGallery.defaultOptions.captionsData = 'alt';
+
+  LightboxGallery.defaultOptions.captionDelay = 250;
+  document.addEventListener('keyup', event => {
+    if (event.code === 'Escape') {
+      LightboxGallery.close;
+    }
+  });
+}
+
+async function fetchPictures() {
+  let params = new URLSearchParams({
+    key: '30974723-e837a19c04863567111943fb7',
+    q: q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: per_page,
+    page: page,
+  });
+  const URL = `https://pixabay.com/api/?${params}`;
   const response = await fetch(`${URL}`);
   if (!response.ok) {
     throw new Error(response.statusText);
@@ -69,6 +93,7 @@ async function fetchPictures(URL) {
   return pictures;
 }
 
+/*const totalPages = pictures.totalHits / params.per_page;*/
 /*loadMore.addEventListener('click', () => {
         params.page = params.page + 1;
         console.log(params.page);
